e790bb4bd2c34a8d480102743ba16f0d40dbf432
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2023-01-10 20:40:22 +0530

    Add files via upload

---
+++ input_ways_basics_1.py
@@ -0,0 +1,12 @@
+a=input()
+print('the value of a is '+a)#the value of a is 1
+b=int(input('enter the 3 numbers:'))#125
+print('the value of b is %i'%b)#the value of b is 125
+c=float(input('enter the number:'))#2.3
+print("the value of c is",c)#the value of c is 2.3
+print('the value of c is %f'%c)#the value of c is 2.300000
+a=6
+print(a)#6
+print(type(a))#int
+g=5.2
+print(g,type(g)) #5.2 float
